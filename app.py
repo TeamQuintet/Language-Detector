44ebabf942fc83438fd515416325b8160a4c48f8
cham2601 <chamanthyarlagadda@gmail.com>
2021-10-20 13:39:35 +0530

    added frontend files

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, redirect, render_template, request, jsonify
 from flask.helpers import make_response, url_for
-import pandas as pd
+# import pandas as pd
 # import tensorflow as tf
 # import keras
 # from keras.models import load_model
@@ -10,17 +10,17 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index2.html")
+    return render_template("index.html")
 
 # model = load_model('myModel.h5')
 
-@app.route("/predict/", methods=["GET","POST"])
-def predict():
-    message = request.form['message']
-    data = [message]
-    # msg = toke
-    prediction = 'English'
-    return render_template("index2.html", prediction)
+@app.route("/detect", methods=["GET", "POST"])
+def predictor():
+    return render_template("detect.html")
+
+@app.route("/translate", methods=["GET", "POST"])
+def translator():
+    return render_template("translate.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
